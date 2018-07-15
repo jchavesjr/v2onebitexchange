5b5d11e36098b1de4324b1b0ed79dbd374cb4350
João Chaves <jochavesjr@gmail.com>
2018-07-15 09:21:05 -0300

    ajustes do teste de sistemas

--- spec/system/pages_index_system_spec.rb
+++ spec/system/pages_index_system_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe "Exchange Currency Process", :type => :system, js: true do
   it "exchange value" do
     visit '/'
     within("#exchange_form") do
-      select('EUR', from: 'source_currency')
-      select('USD', from: 'target_currency')
+      select('BRL', from: 'source_currency')
+      select('EUR', from: 'target_currency')
       fill_in 'amount', with: '10'
-      click_button 'CONVERTER'
+      click_button 'Botão para inverter a moeda'
       select('EUR', from: 'source_currency')
-      select('USD', from: 'target_currency')
+      select('BRL', from: 'target_currency')
     end
     
  
